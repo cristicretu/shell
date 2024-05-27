f549d03f51a859d791deb3aa9d8832d1ab04747e
Cristian Cretu <crisemcr@gmail.com>
2024-05-27 16:46:59 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#[allow(unused_imports)]
+use std::io::{self, Write};
+
+fn main() {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+    }
+}
